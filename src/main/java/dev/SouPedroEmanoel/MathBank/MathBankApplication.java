f4d5dec62dd386fe191c3bba3015569ce0d8dfb6
Pedro Emanoel da Silva de Oliveira <pedroemanoeldasilvadeoliveira@gmail.com>
2025-06-06 10:55:40 -0300

    first commit

---
+++ src/main/java/dev/SouPedroEmanoel/MathBank/MathBankApplication.java
@@ -0,0 +1,13 @@
+package dev.SouPedroEmanoel.MathBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/SouPedroEmanoel/MathBank/MathBankApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.SouPedroEmanoel.MathBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
